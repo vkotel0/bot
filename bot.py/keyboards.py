166c5033838fe66bcb3bc7c9bd79f1d30f903db5
vkotel0 <vladkotelnikov254gmail.com>
2023-06-12 09:51:09 +0300

    message

--- bot.py/keyboards.py
+++ bot.py/keyboards.py
@@ -1,13 +1,21 @@
 from aiogram.types import \
     KeyboardButton, \
     ReplyKeyboardMarkup, \
-    ReplyKeyboardRemove
-    
-b1 = KeyboardButton("Найти собеседника")
-b2 = KeyboardButton("Больше не хочу никого искать")
-b3 = KeyboardButton("🙋‍♂️")
+    ReplyKeyboardRemove, \
+    InlineKeyboardMarkup, \
+    InlineKeyboardButton
+       
+i1 = InlineKeyboardButton('👍', callback_data='i1')
+i2 = InlineKeyboardButton('👎', callback_data='i2')        
 
+inlineKeyboard = InlineKeyboardMarkup().insert(i1).insert(i2)       
+        
+b1 = KeyboardButton("Поделиться номером", request_location=True)
+b2 = KeyboardButton("Больше не хочу никого искать", request_contact=True)
+b3 = KeyboardButton("🙋‍♂️")
 keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard.add(b1).add(b2).add(b3)
 
-keyboard2 = ReplyKeyboardMarkup()
+
+keyboard2 = ReplyKeyboardMarkup(one_time_keyboard=True)
+keyboard2.insert(b1).insert(b2).insert(b3)
